9635ce3bd01a4e9ce051619da817010fe381d56e
kimhyewon92 <sing92.naver.com>
2025-06-09 17:52:07 +0900

    stream homework

---
+++ chap14/src/homework/homework01/Practice.java
@@ -0,0 +1,5 @@
+package homework.homework01;
+
+public class Practice {
+
+}
---
+++ chap14/src/homework/homework01/Run.java
@@ -0,0 +1,10 @@
+package homework.homework01;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
